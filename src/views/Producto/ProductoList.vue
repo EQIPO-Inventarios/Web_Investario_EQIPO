556436e61c6bd6ed40e60505cafd9651c390b866
PlateroJulio <julioplatero98@gmail.com>
2020-11-13 21:35:33 -0600

    CRUD de ProductoSucursales

--- src/views/Producto/ProductoList.vue
+++ src/views/Producto/ProductoList.vue
@@ -1,18 +1,39 @@
 <template>
   <div>
       <menuComponent></menuComponent>
-      <Listado></Listado>
+      <ListadoP v-if="comprobar"></ListadoP>
+      <ListadoN v-if="comprobar == false"></ListadoN>
   </div>
 </template>
 <script>
-import Listado from '@/components/Productos/listarProducto.vue'
+import ListadoP from '@/components/Productos/listarProducto.vue'
+import ListadoN from '@/components/ProductoSucursales/listarProducto.vue'
 import menuComponent from '@/components/menus/menuComponent.vue'
 
 export default {
     name: 'ProductoL',
     components: {
         menuComponent,
-        Listado
+        ListadoP,
+        ListadoN
+    },
+    data(){
+        return {
+            sucursal: sessionStorage.getItem('nomSucursal'),
+            comprobar: false
+        }
+    },
+    mounted() {
+        this.EsPrincipal();
+    },
+    methods: {
+        EsPrincipal(){
+            if (this.sucursal == 'Sucursal Principal') {
+                this.comprobar = true;
+            } else {
+                this.comprobar = false;
+            }   
+        }
     }
 }
 </script>
